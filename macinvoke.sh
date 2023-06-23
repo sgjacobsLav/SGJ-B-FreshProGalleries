65e6aa8fd34494a76b29e458778242865ef2bda8
Shmuel Gary Jacobs <sgjacobs.write@gmail.com>
2023-06-22 20:38:42 -0700

    Fix main script path for git managed install

--- macinvoke.sh
+++ macinvoke.sh
@@ -1,4 +1,4 @@
 python3 -m poetry lock
 python3 -m poetry run python \
-    ~/Documents/SGJ-B-FreshProGalleries-feature-initial-feature/main.py \
+    ~/Documents/SGJ-B-FreshProGalleries/main.py \
     --mapping_file mappings.json
